3e3a4fc86975d987912792eabed340b8bce57c9a
PatrickDuffin1 <54854953+PatrickDuffin1@users.noreply.github.com>
2024-06-24 14:23:31 +0100

    Update arrival_report.js

--- arrival_report.js
+++ arrival_report.js
@@ -1,143 +1,105 @@
 // Include custom icons and styles
-const styles = `
+const icons = `
 <style>
-  .meta-container {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
+  .arrival-card {
+    border-radius: 10px;
+    padding: 20px;
     width: 100%;
     box-sizing: border-box;
-  }
-  .persona-breakdown {
-    display: flex;
-    align-items: center;
-    font-size: 1.5vw;
-    margin-bottom: 10px;
-  }
-  .circle {
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    font-family: Arial, sans-serif;
     font-size: 4vw;
-    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%; /* Ensure full height usage */
+  }
+  .arrival-header, .arrival-details, .arrival-details-nights {
+    flex: 1;
+  }
+  .arrival-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .arrival-header .status {
+    background: #ffb921;
+    padding: 5px 10px;
+    border-radius: 10px;
     color: white;
+    font-weight: bold;
   }
-  .big-circle {
-    background-color: #4D6EBF;
-    width: 10vw;
-    height: 10vw;
-    flex-shrink: 0;
-  }
-  .small-circle {
-    background-color: #EAEAEA;
-    color: black;
-    width: 5vw;
-    height: 5vw;
-    margin-left: 10px;
-    flex-shrink: 0;
-  }
-  .text-content {
+  .arrival-details div {
     display: flex;
     align-items: center;
-    margin-left: 10px;
-    justify-content: space-between;
-    flex-grow: 1;
   }
-  .metric-name {
-    margin-right: 5px;
+  .icon {
+    margin-right: 10px;
+    width: 1em; /* Set icon width based on the font size */
+    height: 1em; /* Set icon height based on the font size */
   }
 </style>
 `;
-
 looker.plugins.visualizations.add({
-  create: function (element, config) {
+  create: function(element, config) {
     element.style.fontFamily = `"Open Sans", "Helvetica", sans-serif`;
-    element.innerHTML = styles;
   },
-  updateAsync: function (data, element, config, queryResponse, details, done) {
+  updateAsync: function(data, element, config, queryResponse, details, done) {
     element.innerHTML = ''; // Clear any existing content
-    element.innerHTML += styles;
-
-    const fields = [...queryResponse.fields.dimension_like, ...queryResponse.fields.measure_like];
-    const items = fields.slice(0, maxFields);
-
-    // const fields = queryResponse.fields.dimension_like;
-    // const metrics = [
-    //   {label: "Solo Traveler", value: Number(fields[0].value)},
-    //   {label: "Couple Travel", value: Number(fields[1].value)},
-    //   {label: "Family Vacation", value: Number(fields[2].value)},
-    //   {label: "Festive Traveller", value: Number(fields[3].value)},
-    //   {label: "Summer Traveller", value: Number(fields[4].value)},
-    //   {label: "Weekend Traveller", value: Number(fields[5].value)}
-    // ];
-
-    // Sort metrics based on the value in descending order
-    metrics.sort((a, b) => b.value - a.value);
-    
-    // Calculate the total sum of metrics
-    const total = metrics.reduce((sum, metric) => sum + metric.value, 0);
-    
-    const container = document.createElement('div');
-    container.classList.add('meta-container');
-    
-    // Creating the big circle
-    const bigCircleContent = `
-      <div class="circle big-circle">
-        ${metrics[0].value}
-        <div style="font-size: 2vw;">${metrics[0].label}</div>
-        <div style="font-size: 1vw;">${((metrics[0].value / total) * 100).toFixed(2)}%</div>
-      </div>
-    `;
-    
-    const bigCircleContainer = document.createElement('div');
-    bigCircleContainer.classList.add('persona-breakdown');
-    bigCircleContainer.innerHTML = bigCircleContent;
-    container.appendChild(bigCircleContainer);
-    
-    // Creating small circles and corresponding texts
-    for (let i = 1; i < metrics.length; i++) {
-      const smallCircleContent = `
-        <div class="circle small-circle">
-          ${metrics[i].value}
-        </div>
-        <div class="text-content">
-          <div class="metric-name">${metrics[i].label}</div>
-          <div>${((metrics[i].value / total) * 100).toFixed(2)}%</div>
-        </div>
-      `;
-      const smallCircleContainer = document.createElement('div');
-      smallCircleContainer.classList.add('persona-breakdown');
-      smallCircleContainer.innerHTML = smallCircleContent;
-      container.appendChild(smallCircleContainer);
+    // Append CSS styles to the element
+    element.innerHTML += icons;
+    // Extracting fields in the specific order
+    const fields = queryResponse.fields.dimension_like;
+    // Check if there are at least 7 fields available
+    if (fields.length < 7) {
+      element.innerHTML += 'Insufficient data fields';
+      done();
+      return;
     }
-    items.forEach((field, index) => {
-      const fieldName = field.name;
-      const fieldLabel = field.label_short || field.label;
-      const fieldValue = data[0][fieldName].rendered || data[0][fieldName].value || '∅';
+    // Extract data from the first row in the order specified
+    const row = data[0];
+    const due_in_time = row[fields[0].name].value;
+    const location = row[fields[1].name].value;
+    const start_date = row[fields[2].name].value;
+    const end_date = row[fields[3].name].value;
+    const num_nights = row[fields[4].name].value;
+    const num_guests = row[fields[5].name].value;
+    const room_numbers = row[fields[6].name].value;
 
-      const vizElement = document.createElement('div');
-      vizElement.className = 'viz-element';
+    // Calculate dynamic line height based on container height
+    const containerHeight = element.clientHeight;
+    const lineHeight = containerHeight / 26; // Adjust this value to distribute contents properly
+    const statusLineHeight = lineHeight * 0.4; // Adjust this value to distribute contents properly
 
-      const metricColor = config[`metric${index + 1}_color`] || config.master_color;
-      const metricTitle = config[`metric${index + 1}_title`] || fieldLabel;
+    // Construct the arrival card with dynamic line height
+    const arrivalCard = `
+      <div class="arrival-card" style="line-height: ${lineHeight}vw;">
+        <div class="arrival-header">
+          <div>Arrival Report</div>
+          <div class="status" style="line-height: ${statusLineHeight}vw;">Due in - ${due_in_time}</div>
+        </div>
+        <div class="arrival-details"">
+          <div><strong>${location}</strong></div>
+            <div class="arrival-details-nights" style="justify-content: space-between;">
+              <div>
+                <img class="icon" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yNVwvMTcyMDUwXC9wcm9qZWN0c1wvMjc4Mjc0XC9hc3NldHNcLzJiXC80OTk5MTQ0XC83MDVmMzYzMGFhMTM1NTcxYTAzYzNmYzk3ODE4MDVmMi0xNjA3NjIyMzc4LnN2ZyJ9:frontify:DsE91qZoxdtRg4QXzR3qxmhTvGoA4k703e74VvXnx6Q?width=2400"></img>
+                ${start_date} - ${end_date}</div>
+                <div class="nights"><img class="icon" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yNVwvMTcyMDUwXC9wcm9qZWN0c1wvMjc4Mjc0XC9hc3NldHNcL2Y4XC80OTk5MDU4XC9hM2MzM2MyZmVmZTYyYTQ3NTBlZDkzZDI2ODc0YWNkNC0xNjA3NjIyMTkyLnN2ZyJ9:frontify:cLkhh1-xYamDkYHdQWOtmVNKFRElcvUisK6PfG7JUk0?width=2400"></img> ${num_nights}</div>
+              </div>
+            <div>
+              <img class="icon" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yNVwvMTcyMDUwXC9wcm9qZWN0c1wvMjc4Mjc0XC9hc3NldHNcLzMzXC80OTk5MDY5XC84NTM1MzZiMTJmM2YwMDc3YTVjNmEyM2Q1YzIwYjZiYS0xNjA3NjIyMTkyLnN2ZyJ9:frontify:XKUzuk-yTmtyqPmtN1vMYqipmXGVfqtUtmLso-gWDxM?width=2400"></img>
+              ${num_guests} guests
+            </div>
+        <div>
+          <img class="icon" src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yNVwvMTcyMDUwXC9wcm9qZWN0c1wvMjc4Mjc0XC9hc3NldHNcL2ZkXC80OTk5Mzc2XC9kMGUyN2I1Mzg0MTgxOTEzNTUwOWY4ZmU3YmY2NjkwNS0xNjA3NjIyNzI5LnN2ZyJ9:frontify:EPBkv4IFrfiYi5C_oehea7Jr30rluxP8qKl5Ab1WN3k?width=2400"></img>
+          Rooms ${room_numbers}
+        </div>
+      </div>
+    </div>
+    `;
 
-      const valueElement = document.createElement('div');
-      valueElement.className = 'viz-value';
-      valueElement.innerHTML = fieldValue;
-      valueElement.style.color = metricColor;
+    // Append the card to the element
+    element.innerHTML += arrivalCard;
 
-      const titleElement = document.createElement('div');
-      titleElement.className = 'viz-title';
-      titleElement.innerText = metricTitle;
-      titleElement.style.color = metricColor;
-
-      vizElement.appendChild(valueElement);
-      vizElement.appendChild(titleElement);
-      vizContainer.appendChild(vizElement);
-    });
-    
-    element.appendChild(container);
     done();
   }
 });
