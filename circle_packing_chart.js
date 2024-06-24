671967fcee879542eeae197d0eb695057e6bdc96
PatrickDuffin1 <54854953+PatrickDuffin1@users.noreply.github.com>
2024-06-24 16:55:29 +0100

    Update circle_packing_chart.js

--- circle_packing_chart.js
+++ circle_packing_chart.js
@@ -21,29 +21,42 @@ looker.plugins.visualizations.add({
       order: 3
     }
   },
-  
-  // Dynamically add options for custom labels and icons
-  updateDynamicOptions: function(config, queryResponse) {
-    const fields = [...queryResponse.fields.dimension_like, ...queryResponse.fields.measure_like];
-    fields.forEach((field, index) => {
-      this.options[`metric_label_${index}`] = {
-        type: "string",
-        label: `Label for Metric ${index + 1}`,
-        default: "",
-        order: 4 + index * 2
-      };
-      this.options[`metric_icon_${index}`] = {
-        type: "string",
-        label: `Icon URL for Metric ${index + 1}`,
-        default: "",
-        order: 5 + index * 2
-      };
-    });
-  },
-  
+
   create: function(element, config) {
     element.style.fontFamily = `"Lato", sans-serif`;
-    element.innerHTML = `
+  },
+
+  updateDynamicOptions: function(queryResponse) {
+    const numFields = queryResponse.fields.dimension_like.length + queryResponse.fields.measure_like.length;
+    // Clear existing dynamic options
+    Object.keys(this.options).forEach((key) => {
+      if (key.startsWith("metric_label_") || key.startsWith("metric_icon_")) {
+        delete this.options[key];
+      }
+    });
+    // Add dynamic options for each metric
+    for (let i = 0; i < numFields; i++) {
+      this.options[`metric_label_${i}`] = {
+        type: "string",
+        label: `Label for Metric ${i + 1}`,
+        default: "",
+        order: 4 + i * 2
+      };
+      this.options[`metric_icon_${i}`] = {
+        type: "string",
+        label: `Icon URL for Metric ${i + 1}`,
+        default: "",
+        order: 5 + i * 2
+      };
+    }
+    this.trigger('registerOptions', this.options);
+  },
+
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    // Update dynamic options
+    this.updateDynamicOptions(queryResponse);
+
+    const styleContent = `
       <style>
         .big-circle {
           display: flex;
@@ -73,6 +86,11 @@ looker.plugins.visualizations.add({
           color: white;
           margin-right: 10px;
         }
+        .metric-callout {
+          display: flex;
+          align-items: center;
+          margin-left: 10px;
+        }
         .metric-name, .metric-percentage {
           margin-left: 5px;
           color: grey;
@@ -84,54 +102,9 @@ looker.plugins.visualizations.add({
         }
       </style>
     `;
-  },
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Update dynamic options based on the data
-    this.updateDynamicOptions(config, queryResponse);
 
     // Clear the content
-    element.innerHTML = ''; 
-    element.innerHTML += `
-      <style>
-        .big-circle {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          border-radius: 50%;
-          width: 30vw;
-          height: 30vw;
-          color: white;
-          text-align: center;
-        }
-        .big-circle-icon {
-          max-width: 10vw;
-          max-height: 10vw;
-          margin-bottom: 10px;
-        }
-        .metric-block {
-          display: flex;
-          align-items: center;
-          margin-top: 10px;
-        }
-        .small-circle {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          border-radius: 50%;
-          color: white;
-          margin-right: 10px;
-        }
-        .metric-name, .metric-percentage {
-          margin-left: 5px;
-          color: grey;
-        }
-        .small-circle-icon {
-          max-width: 3vw; 
-          max-height: 3vh; 
-          padding-right: 0.5vw;
-        }
-      </style>
-    `;
+    element.innerHTML = styleContent;
 
     const fields = [...queryResponse.fields.dimension_like, ...queryResponse.fields.measure_like];
 
@@ -173,19 +146,16 @@ looker.plugins.visualizations.add({
     bigCircleContainer.innerHTML = bigCircleContent;
     container.appendChild(bigCircleContainer);
 
-    const metricsContainer = document.createElement('div');
-    metricsContainer.classList.add('metrics-container');
-
     for (let i = 1; i < metrics.length; i++) {
-      const sizePercentage = (metrics[i].value / maxMetricValue) * 30;
+      const sizePercentage = (metrics[i].value / maxMetricValue) * 30; // Relative to 30vw of the big circle
       const fontSizePercentage = sizePercentage * 0.3;
 
       const calloutContent = `
-        <div class="metric-block">
+        <div class="metric-block" style="flex-direction:column; align-items:center;">
           <div class="small-circle" style="width: ${sizePercentage}vw; height: ${sizePercentage}vw; font-size: ${fontSizePercentage}vw; background-color: ${config.small_circle_color};">
             ${metrics[i].value}
           </div>
-          <div class="metric-callout">
+          <div class="metric-callout" style="margin-left:0; flex-direction:column; align-items:center;">
             ${metrics[i].icon ? `<img class="small-circle-icon" src="${metrics[i].icon}">` : ''}
             <div class="metric-name">${metrics[i].label}</div>
             <div class="metric-percentage">${((metrics[i].value / total) * 100).toFixed(config.decimal_places)}%</div>
@@ -195,10 +165,9 @@ looker.plugins.visualizations.add({
       const calloutContainer = document.createElement('div');
       calloutContainer.classList.add('metric-block');
       calloutContainer.innerHTML = calloutContent;
-      metricsContainer.appendChild(calloutContainer);
+      container.appendChild(calloutContainer);
     }
 
-    container.appendChild(metricsContainer);
     element.appendChild(container);
     done();
   }
