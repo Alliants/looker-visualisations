1250555b53447b632a0e86d417abd4b19269eb39
PatrickDuffin1 <54854953+PatrickDuffin1@users.noreply.github.com>
2024-06-25 16:25:58 +0100

    Update metric_icon_tile.js

--- metric_icon_tile.js
+++ metric_icon_tile.js
@@ -20,7 +20,7 @@ looker.plugins.visualizations.add({
     const numFields = fields.length;
 
     // Remove previous dynamic options
-    Object.keys(this.options || {}).forEach((key) => {
+    Object.keys(this.options).forEach((key) => {
       if (key.startsWith('metric_color_') || key.startsWith('icon_url_') || key.startsWith('metric_label_')) {
         delete this.options[key];
       }
@@ -94,10 +94,11 @@ looker.plugins.visualizations.add({
         const field = fields[metricIndex];
         const fieldName = field.name.replace(/\./g, '_');
         const fieldLabel = config[`metric_label_${fieldName}`] || field.label_short || field.label;
-        const fieldValue = (data[0][field.name] && (data[0][field.name].rendered || data[0][field.name].value)) || '∅';
+        const fieldValue = data[0][field.name].rendered || data[0][field.name].value || '∅';
         const iconURL = config[`icon_url_${fieldName}`] || '';
         const metricColor = config[`metric_color_${fieldName}`] || config.master_color;
         const iconColor = this.hexToRgb(metricColor);
+
 
         const metricContainer = document.createElement('div');
         metricContainer.className = 'metric-container';
@@ -107,14 +108,14 @@ looker.plugins.visualizations.add({
         valueElement.className = 'metric-value';
         valueElement.innerText = fieldValue;
         valueElement.style.color = metricColor;
-        valueElement.style.fontSize = '5vw';
+        valueElement.style.fontSize = `${containerWidth / 20}px`;
         metricContainer.appendChild(valueElement);
 
         if (iconURL) {
           const iconElement = document.createElement('img');
-          iconElement.src = `${iconURL}&color=${iconColor}`;
-          iconElement.style.width = '10vw';
-          iconElement.style.height = '10vh';
+          iconElement.src = `${iconURL}&color=${iconColor},1`;
+          iconElement.style.width = `${containerWidth / 10}px`;
+          iconElement.style.height = `${containerHeight / 10}px`;
           metricContainer.appendChild(iconElement);
         }
 
@@ -122,7 +123,7 @@ looker.plugins.visualizations.add({
         labelElement.className = 'metric-label';
         labelElement.innerText = fieldLabel;
         labelElement.style.color = metricColor;
-        labelElement.style.fontSize = '2.5vw';
+        labelElement.style.fontSize = `${containerWidth / 30}px`;
         metricContainer.appendChild(labelElement);
 
         row.appendChild(metricContainer);
