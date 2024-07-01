0f7eaaf11af4f1ff56f3cd86573a405d9b0dd1ea
PatrickDuffin1 <54854953+PatrickDuffin1@users.noreply.github.com>
2024-07-01 16:39:28 +0100

    Update circle_packing_chart.js

--- circle_packing_chart.js
+++ circle_packing_chart.js
@@ -1,70 +1,72 @@
 looker.plugins.visualizations.add({
   options: {
     decimal_places: {
-      type: 'number',
-      label: 'Decimal Places for Percentages',
+      type: "number",
+      label: "Decimal Places for Percentages",
       default: 2,
       order: 1
     },
     big_circle_color: {
-      type: 'string',
-      label: 'Big Circle Color',
-      default: '#4D6EBF',
-      display: 'color',
+      type: "string",
+      label: "Big Circle Color",
+      default: "#4D6EBF",
+      display: "color",
       order: 2
     },
     big_circle_font_color: {
-      type: 'string',
-      label: 'Big Circle Font Color',
-      default: '#FFFFFF',
-      display: 'color',
+      type: "string",
+      label: "Big Circle Font Color",
+      default: "#FFFFFF",
+      display: "color",
       order: 3
     },
     small_circle_color: {
-      type: 'string',
-      label: 'Small Circle Color',
-      default: '#EAEAEA',
-      display: 'color',
+      type: "string",
+      label: "Small Circle Color",
+      default: "#EAEAEA",
+      display: "color",
       order: 4
     },
     small_circle_font_color: {
-      type: 'string',
-      label: 'Small Circle Font Color',
-      default: '#000000',
-      display: 'color',
+      type: "string",
+      label: "Small Circle Font Color",
+      default: "#000000",
+      display: "color",
       order: 5
     }
   },
 
-  create: function (element, config) {
+  create: function(element, config) {
     element.style.fontFamily = 'Lato, sans-serif';
   },
 
-  updateDynamicOptions: function (queryResponse) {
+  updateDynamicOptions: function(queryResponse) {
     const numFields = queryResponse.fields.dimension_like.length + queryResponse.fields.measure_like.length;
+    // Clear existing dynamic options
     Object.keys(this.options).forEach((key) => {
-      if (key.startsWith('metric_label_') || key.startsWith('metric_icon_')) {
+      if (key.startsWith("metric_label_") || key.startsWith("metric_icon_")) {
         delete this.options[key];
       }
     });
+    // Add dynamic options for each metric
     for (let i = 0; i < numFields; i++) {
       this.options[`metric_label_${i}`] = {
-        type: 'string',
+        type: "string",
         label: `Label for Metric ${i + 1}`,
-        default: '',
+        default: "",
         order: 6 + i * 2
       };
       this.options[`metric_icon_${i}`] = {
-        type: 'string',
+        type: "string",
         label: `Icon URL for Metric ${i + 1}`,
-        default: '',
+        default: "",
         order: 7 + i * 2
       };
     }
     this.trigger('registerOptions', this.options);
   },
 
-  hexToRgb: function (hex) {
+  hexToRgb: function(hex) {
     hex = hex.replace('#', '');
     const bigint = parseInt(hex, 16);
     const r = (bigint >> 16) & 255;
@@ -73,24 +75,26 @@ looker.plugins.visualizations.add({
     return `${r},${g},${b}`;
   },
 
-  updateAsync: function (data, element, config, queryResponse, details, done) {
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    // Update dynamic options
     this.updateDynamicOptions(queryResponse);
 
+    // Remove any existing content
     element.innerHTML = '';
 
     const fields = [...queryResponse.fields.dimension_like, ...queryResponse.fields.measure_like];
-    const that = this;
+    const that = this; // To avoid scope issues inside map function
 
     const metrics = fields.map((field, index) => {
       const value = data[0][field.name]?.value;
       const label = config[`metric_label_${index}`] || field.label_short || field.label;
-      const icon = config[`metric_icon_${index}`] || '';
+      const icon = config[`metric_icon_${index}`] || "";
       return {
         label: label,
         value: isNaN(value) ? 0 : Number(value),
         icon: icon
       };
-    }).filter((metric) => metric.value !== 0);
+    }).filter(metric => metric.value !== 0);
 
     metrics.sort((a, b) => b.value - a.value);
 
@@ -110,6 +114,7 @@ looker.plugins.visualizations.add({
     container.style.height = '100%';
     container.style.width = '100%';
 
+    // Create the big circle for the largest metric
     const bigCircleIconColor = this.hexToRgb(config.big_circle_font_color);
     const bigCircle = document.createElement('div');
     const strongLabel = document.createElement('strong');
@@ -126,7 +131,7 @@ looker.plugins.visualizations.add({
     bigCircle.style.textAlign = 'center';
     bigCircle.style.fontSize = '5vw';
     bigCircle.style.padding = '2vw';
-    bigCircle.style.color = `${config.big_circle_font_color}`;
+    bigCircle.style.color = `${config.big_circle_font_color}`;    
     const bigCircleImg = document.createElement('img');
     bigCircleImg.className = 'icon';
     bigCircleImg.src = `${metrics[0].icon}&color=${bigCircleIconColor},1`;
@@ -134,6 +139,11 @@ looker.plugins.visualizations.add({
     bigCircleImg.style.height = '15vh';
     bigCircle.appendChild(bigCircleImg);
     bigCircle.appendChild(strongLabel);
+    
+    // bigCircle.innerHTML = `
+    //     <div>${metrics[0].icon ? `<img class="big-circle-icon" src="${metrics[0].icon}&color=${bigCircleIconColor},1" style="width: 15vw; height: 15vh;">` : ''}</div>
+    //     <div style="color: ${config.big_circle_font_color};"><strong>${metrics[0].value} ${metrics[0].label}</strong></div>
+    // `;
 
     const bigCircleContainer = document.createElement('div');
     bigCircleContainer.classList.add('big-circle-container');
@@ -149,13 +159,14 @@ looker.plugins.visualizations.add({
     metricsContainer.style.display = 'flex';
     metricsContainer.style.flexDirection = 'column';
     metricsContainer.style.justifyContent = 'space-evenly';
-    metricsContainer.style.height = `${bigCircleDiameter}vw`;
+    metricsContainer.style.height = `${bigCircleDiameter}vw`; // Match height with big circle
     metricsContainer.style.marginLeft = '20px';
 
     for (let i = 1; i < metrics.length; i++) {
-      const sizePercentage = (metrics[i].value / maxMetricValue) * bigCircleDiameter;
+      const sizePercentage = (metrics[i].value / maxMetricValue) * bigCircleDiameter; // Relative to 30vw of the big circle
       const smallCircleIconColor = this.hexToRgb(config.small_circle_font_color);
 
+      // Create small circles for the rest of the metrics
       const smallCircle = document.createElement('div');
       smallCircle.classList.add('small-circle');
       smallCircle.style.width = `${sizePercentage}vw`;
@@ -166,48 +177,21 @@ looker.plugins.visualizations.add({
       smallCircle.style.justifyContent = 'center';
       smallCircle.style.alignItems = 'center';
       smallCircle.style.borderRadius = '50%';
-      smallCircle.style.maxWidth = '25vw';
-      smallCircle.style.maxHeight = '25vw';
-
-      metricsContainer.appendChild(smallCircle);
-      
-      // Append the small-circle element to ensure it exists in the DOM
-      const metricBlock = document.createElement('div');
-      metricBlock.classList.add('metric-block');
-      metricBlock.style.display = 'flex';
-      metricBlock.style.alignItems = 'center';
-      metricBlock.appendChild(smallCircle);
-      metricsContainer.appendChild(metricBlock);
-
-      // Ensure the small circle element exists in the DOM
-      console.log("Small circle element:", smallCircle);
-
-      // Get computed styles
-      const computedStyle = getComputedStyle(smallCircle);
-      console.log("Computed style:", computedStyle);
-
-      // Get the width in pixels directly from the computedStyle
-      const widthInPx = parseFloat(computedStyle.width);
-      console.log("Width in px:", widthInPx);
-
-      // Calculate radius in pixels
-      const radiusInPx = widthInPx / 2;
-      console.log("Radius in px:", radiusInPx);
-
-      // Use the radius to determine the line width
-      const lineWidth = widthInPx * 1.05;
-      console.log("Line width:", lineWidth);
+      smallCircle.style.maxWidth = `25vw`;
+      smallCircle.style.maxHeight = `25vw`;
+      // smallCircle.textContent = metrics[i].value;
 
       const lineContainer = document.createElement('div');
+      const lineOffset = sizePercentage * 0.5;
       lineContainer.classList.add('line-container');
       lineContainer.style.display = 'flex';
       lineContainer.style.alignItems = 'center';
-      lineContainer.style.marginLeft = `-${radiusInPx}px`;
+      lineContainer.style.marginLeft = `-${lineOffset}vw`;
 
       const line = document.createElement('div');
-      line.classList.add('line');
+      lineContainer.classList.add('line');
       line.style.height = '1px';
-      line.style.width = `${lineWidth}px`;
+      line.style.width = '10vw';
       line.style.backgroundColor = 'rgb(71, 71, 71)';
 
       lineContainer.appendChild(line);
@@ -241,7 +225,13 @@ looker.plugins.visualizations.add({
 
       metricName.appendChild(label);
       metricCallout.appendChild(metricName);
+      metricsContainer.appendChild(metricCallout);
 
+      const metricBlock = document.createElement('div');
+      metricBlock.classList.add('metric-block');
+      metricBlock.style.display = 'flex';
+      metricBlock.style.alignItems = 'center';
+      metricBlock.appendChild(smallCircle);
       metricBlock.appendChild(lineContainer);
       metricBlock.appendChild(metricCallout);
       metricsContainer.appendChild(metricBlock);
